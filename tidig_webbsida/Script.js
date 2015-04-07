60c60d3df0cff3d262491a630cd24c24bac09a97
Hannes <hannesbirgersson@gmail.com>
2015-04-07 11:42:52 +0200

    JavaScript removeItem
    removeItem funktion för borttagning av enstaka objekt

--- tidig_webbsida/Script.js
+++ tidig_webbsida/Script.js
@@ -7,9 +7,11 @@ function setUp() {
     document.getElementById("rensa").onclick = rensa;
     if (document.body.addEventListener) {
         document.body.addEventListener('click',addItem,false);
+		document.body.addEventListener('click',removeItem,false);
     }
     else {
         document.body.attachEvent('onclick',addItem);
+		document.body.attachEvent('onclick',removeItem);
     }
 	
 }
@@ -39,13 +41,25 @@ function addItem(e) {
         }
         ingrds.push(target.innerHTML)
         var div = document.getElementById('Ingredienslista');
-        div.innerHTML += "<li>" + target.innerHTML + '<span><a href="javaScript:void(0);">X</a></span></li>';
+        div.innerHTML += "<span><li>" + target.innerHTML + '<span id="cross"><a href="javaScript:void(0);" class="RemoveCross">X</a></span></li></span>';
         return true;
     } 
 }
 
 function removeItem(e) {
-    
+    e = e || window.event;
+    var target;
+    target = e.target || e.srcElement;
+    if (target.className.match(/\bRemoveCross\b/)) {
+		var fullRow = target.parentNode.parentNode.parentNode.innerHTML;
+		var part = fullRow.substring(4,fullRow.lastIndexOf("<span id"));
+		if (ingrds.indexOf(part) != -1) {
+			var index = ingrds.indexOf(part);
+			ingrds.splice(index,1);
+			target.parentNode.parentNode.parentNode.innerHTML = "";
+		} else {return false;}
+        return true;
+    } 
 }
 
 
