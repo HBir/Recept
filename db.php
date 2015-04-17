912891947485af195f289c59229fd5ac6eb69050
kungp <kung.pung@gmail.com>
2015-04-17 21:20:54 +0200

    Bytte add.php mot manage.php där man kan pilla lite mer, förberett med Prepared Statements mot SQL injections och ändrat lite tillfälligt och godtyckligt i db-strukturen så ingredienser har ett eget table.

--- db.php
+++ db.php
@@ -6,59 +6,53 @@
 			$this->open('test2.db');
 		}
 
-		function init_table()
+		function execute($sql, $message)
 		{
-			$sql = <<< EOF
-				CREATE TABLE Recipes
-				(Name          TEXT NOT NULL,
-				Picture        TEXT NOT NULL,
-				Ingredients    TEXT NOT NULL,
-				Text           TEXT NOT NULL,
-				Rating          INT);
-EOF;
-
 			$ret = $this->exec($sql);
 			if(!$ret){
 				echo $this->lastErrorMsg();
 			} else {
-				echo "<br>Created table";
+				echo "<br>" . $message;
 			}
 		}
-		
+		// Tömmer databasen.
+		function drop_tables()
+		{
+			$sql = <<<SQL
+				DROP TABLE Recipes;
+				DROP TABLE Ingredients;
+				DROP TABLE RecipesIngredients;
+SQL;
+			$this->execute($sql, "Dropped tables.");
+		}
+
+		function init_tables()
+		{
+			$sql = <<<SQL
+				CREATE TABLE Recipes
+				(Name          TEXT NOT NULL,
+				Picture        TEXT NOT NULL,
+				Text           TEXT NOT NULL,
+				Rating          INT);
+				
+				CREATE TABLE Ingredients
+				(Ingredient TEXT NOT NULL UNIQUE,
+				Category    TEXT);
+
+				CREATE TABLE RecipesIngredients
+				(RecipeID INT NOT NULL,
+				Ingredient TEXT NOT NULL);
+SQL;
+			$this->execute($sql, "Created tables.");
+		}
+
 		// Returnerar receptet med $id.
 		function get_recipe($id)
 		{
 			$ret = $this->query("SELECT rowid, * FROM Recipes WHERE rowid = " . $id);
 			return $ret->fetchArray();
 		}
-		
-		// Temp-funktion för att lägga till lite testdata.
-		function populate()
-		{
-			$sql = <<< EOF
-			INSERT INTO Recipes VALUES('Mat', 'img/bild.jpg', 'ost', 'tillaga osten', 3);
-			INSERT INTO Recipes VALUES('Mat2', 'img/bild2.jpg', 'skinka', '&auml;t l&aring;ngsamt', 5);
-EOF;
-			$ret = $this->exec($sql);
-			if(!$ret){
-				echo $this->lastErrorMsg();
-			} else {
-				echo "<br>Inserted";
-			}
-		}
 	} 
-
-
-	$db = new DB();
-	if(!$db){
-		echo $db->lastErrorMsg();
-	} else {
-		//echo "Opened database<br>\n\n";
-	}
-
-	//$db->init_table();
-	//$db->populate();
-
 	/* Exempelgrejer
 	$huhu = $db->query("SELECT rowid, * FROM Recipes WHERE Rating > 2");
 
@@ -68,5 +62,5 @@ EOF;
 	}
 
 	echo $db->get_recipe(2)["Name"]; 
-	$db->close(); */
+	*/
 ?>
---
+++ manage.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+	.table { padding:10px; width:900px; }
+	.left { float:left; width:450px; }
+	.right { float:right; width:450px; }
+	input { width:80%; }
+	textarea { width:80%; height:400px; }
+</style>
+</head>
+<?php
+	include("db.php");
+
+	$db = new DB();
+	if(!$db){
+		echo $db->lastErrorMsg();
+		exit();
+	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// Lägga till recept
+		if ($_POST["addtype"] == 1) {
+			$q = $db->prepare("INSERT INTO Recipes VALUES(:name,:image,:recipe,:rating)");
+			$q->bindValue(':name', $_POST["name"], SQLITE3_TEXT);
+			$q->bindValue(':image', $_POST["pic"], SQLITE3_TEXT);
+			$q->bindValue(':recipe', $_POST["recipe"], SQLITE3_TEXT);
+			$q->bindValue(':rating', $_POST["rating"], SQLITE3_INTEGER);
+			$ret = $q->execute();
+			if(!$ret){
+				echo $db->lastErrorMsg();
+			}
+
+			$id = $db->lastInsertRowID();
+			$ingredients = explode(PHP_EOL, mb_strtolower($_POST["ingredients"], 'UTF-8'));
+			
+			// Gå genom alla ingredienser och lägg till i Ingredients, + en koppling till recept-id
+			// i RecipesIngredients.
+			foreach ($ingredients as $i) {
+				if ($i !== '') {
+					// Lägg till ingrediensen:
+					$q = $db->prepare("INSERT OR IGNORE INTO Ingredients VALUES(:ingredient,'')");
+					$q->bindValue(':ingredient', $i, SQLITE3_TEXT);
+					$ret = $q->execute();
+
+					if(!$ret){
+						echo $db->lastErrorMsg();
+					}
+					// Lägg till kopplingen recept<->ingrediens:
+					$q = $db->prepare("INSERT INTO RecipesIngredients VALUES(:id,:ingredient)");
+					$q->bindValue(':id', $id, SQLITE3_INTEGER);
+					$q->bindValue(':ingredient', $i, SQLITE3_TEXT);
+					$ret = $q->execute();
+
+					if(!$ret){
+						echo $db->lastErrorMsg();
+					}
+				}
+			}
+		}
+
+		// Lägga till endast ingredienser
+		if ($_POST["addtype"] == 2) {
+			$ingredients = explode(PHP_EOL, mb_strtolower($_POST["ingredients"], 'UTF-8'));
+			foreach ($ingredients as $i) {
+				if ($i !== '') {
+					$q = $db->prepare("INSERT OR IGNORE INTO Ingredients VALUES(:ingredient,'')");
+					$q->bindValue(':ingredient', $i, SQLITE3_TEXT);
+					$ret = $q->execute();
+
+					if(!$ret){
+						echo $db->lastErrorMsg();
+					}
+				}
+			}
+			echo "Lade till ingrediens(er).";
+		}
+
+		// Nuka databasen och lägg till tables igen (om schemat ändras t ex)
+		if ($_POST["addtype"] == 3) {
+			$db->drop_tables();
+			$db->init_tables();
+		}
+	}
+	echo "<br>Antal recept: " . $db->query("SELECT COUNT(*) FROM Recipes")->fetchArray()[0];
+	echo "<br>Antal ingredienser: " . $db->query("SELECT COUNT(*) FROM Ingredients")->fetchArray()[0];
+	$db->close();
+?>
+<div class="table">
+	<div class="left">
+		<h2>Lägg till recept</h2>
+		<form method="post">
+			<input type="text" name="name" id="name" placeholder="Namn"><br>
+			<input type="text" name="pic" id="pic" placeholder="Bild (filnamn)"><br>
+			<textarea name="ingredients" id="ingredients" placeholder="Ingredienser"></textarea><br>
+			<input type="text" name="recipe" id="recipe" placeholder="Receptet"><br>
+			<input type="number" name="rating" min="1" max="5"><br>
+			<input type="hidden" name="addtype" value="1">
+			<input type="submit" value="Klar" style="width:100px;">
+		</form>
+	</div>
+	<div class="right">
+		<h2>Lägg till ingredienser</h2>
+		<form method="post">
+			<textarea name="ingredients"></textarea><br>
+			<input type="hidden" name="addtype" value="2">
+			<input type="submit" value="Klar" style="width:100px;">
+		</form>
+		<h2>Rensa databasen</h2>
+		<form method="post">
+			<input type="hidden" name="addtype" value="3">
+			<input type="submit" value="Rensa" style="width:100px;">
+		</form>
+	</div>
+</div>
+</html>
